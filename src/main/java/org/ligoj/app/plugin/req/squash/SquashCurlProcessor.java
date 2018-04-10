642a8882d0fc517c2adbc6beeaf70e3fbbf20775
Fabrice Daugan <fabrice.daugan@gmail.com>
2018-04-10 12:26:01 +0200

    Add license headers

--- src/main/java/org/ligoj/app/plugin/req/squash/SquashCurlProcessor.java
+++ src/main/java/org/ligoj/app/plugin/req/squash/SquashCurlProcessor.java
@@ -1,3 +1,6 @@
+/*
+ * Licensed under MIT (https://github.com/ligoj/ligoj/blob/master/LICENSE)
+ */
 package org.ligoj.app.plugin.req.squash;
 
 import org.ligoj.app.resource.plugin.CurlProcessor;
--- src/main/resources/META-INF/resources/webjars/service/req/squash/nls/fr/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/req/squash/nls/fr/messages.js
@@ -1,3 +1,6 @@
+/*
+ * Licensed under MIT (https://github.com/ligoj/ligoj/blob/master/LICENSE)
+ */
 define({
 	'service:req:squash:project' : 'Projet',
 	'service:req:squash:url' : 'URL',
